bebc94ef6a1e3b6134b850647b0f8180ed41194f
Zheng Zhang <zhang.zheng7@northeastern.edu>
2023-01-29 21:06:08 -0500

    submitted

---
+++ LAB_2/src/Model/Customer.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author zhengzhang
+ */
+public class Customer {
+
+    
+    public String fullName;
+    public int customerId;
+    
+    public Customer(){//contributor
+        
+    }
+    
+    
+    
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the customerId
+     */
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    /**
+     * @param customerId the customerId to set
+     */
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+  
+}
